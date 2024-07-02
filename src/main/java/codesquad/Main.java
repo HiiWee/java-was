d78cc18eb2ca4e4ed6a39454ff98044583d969d5
HiiWee <wpdlzhf159@gmail.com>
2024-07-02 16:02:37 +0900

    refactor: corePoolSize를 고정시킨 스레드 풀을 생성하도록 변경한다.
    - corePoolSize가 가득차면 제한 없는 큐에 요청이 저장됨

--- src/main/java/codesquad/Main.java
+++ src/main/java/codesquad/Main.java
@@ -3,9 +3,7 @@ package codesquad;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Executors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,13 +12,12 @@ public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080); // 8080 포트에서 서버를 엽니다.
+        ServerSocket serverSocket = new ServerSocket(8080);
         log.debug("Listening for connection on port 8080 ....");
 
-        ExecutorService executor = new ThreadPoolExecutor(10, 20, 0, TimeUnit.MICROSECONDS,
-                new LinkedBlockingQueue<>());
+        ExecutorService executor = Executors.newFixedThreadPool(10);
 
-        while (true) { // 무한 루프를 돌며 클라이언트의 연결을 기다립니다.
+        while (true) {
             executor.execute(new ConnectionHandler(serverSocket.accept()));
         }
     }
