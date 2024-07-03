aa25552c62ec3056c8c1e7abc06432fbb3021ab8
HiiWee <wpdlzhf159@gmail.com>
2024-07-03 21:52:31 +0900

    fix: jar에서 실행했을때 file path 유동적으로 찾아오도록 classloader를 이용
    - classloader를 통해 변경된 path에서 static 파일을 가져옴

--- src/main/java/codesquad/ConnectionHandler.java
+++ src/main/java/codesquad/ConnectionHandler.java
@@ -3,23 +3,21 @@ package codesquad;
 import codesquad.http.Headers;
 import codesquad.http.HttpRequest;
 import codesquad.http.HttpResponse;
-import codesquad.http.MessageBody;
+import codesquad.http.ResponseMessageBody;
 import codesquad.http.StatusLine;
 import codesquad.http.type.HeaderType;
 import codesquad.http.type.MimeType;
 import codesquad.http.type.StatusCodeType;
 import codesquad.utils.StringUtils;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URL;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ConnectionHandler implements Runnable {
-
-    private static final String STATIC_RELATIVE_PATH = "src/main/resources/static";
 
     private final Logger log = LoggerFactory.getLogger(getClass());
     private final Socket clientSocket;
@@ -40,17 +38,9 @@ public class ConnectionHandler implements Runnable {
 
             HttpResponse httpResponse = createResponse(StatusCodeType.OK, httpRequest);
 
-            clientOutput.write(httpResponse.createResponseMessage().getBytes());
+            clientOutput.write(httpResponse.getResponseBytes());
         } catch (IOException e) {
-            log.error("요청을 처리할 수 없습니다.");
-            throw new RuntimeException("요청을 처리할 수 없습니다.", e);
-        } finally {
-            try {
-                clientSocket.close();
-            } catch (IOException e) {
-                log.error("클라이언트 소켓을 닫을 수 없습니다.");
-                throw new RuntimeException(e);
-            }
+            log.error("요청을 처리할 수 없습니다.", e);
         }
     }
 
@@ -58,14 +48,15 @@ public class ConnectionHandler implements Runnable {
             throws IOException {
         String requestPath = httpRequest.getRequestPath();
 
-        try (FileInputStream fileInputStream = new FileInputStream(STATIC_RELATIVE_PATH + requestPath)) {
+        URL fileUrl = getClass().getClassLoader().getResource("static" + httpRequest.getRequestPath());
+        try (InputStream inputStream = fileUrl.openStream()) {
             Headers headers = new Headers();
             headers.add(HeaderType.CONTENT_TYPE, MimeType.findMimeValue(StringUtils.getFilenameExtension(requestPath)));
 
             return new HttpResponse(
-                    new StatusLine("HTTP/1.1", statusCodeType),
+                    new StatusLine(httpRequest.getHttpVersion(), statusCodeType),
                     headers,
-                    new MessageBody(fileInputStream.readAllBytes()));
+                    new ResponseMessageBody(inputStream.readAllBytes()));
         }
     }
 }
--- src/test/java/codesquad/http/HttpResponseTest.java
+++ src/test/java/codesquad/http/HttpResponseTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import codesquad.http.type.HeaderType;
 import codesquad.http.type.StatusCodeType;
+import java.io.UnsupportedEncodingException;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -13,15 +14,15 @@ class HttpResponseTest {
     class HttpResponse_객체를_생성하면 {
 
         @Test
-        void HttpResponse_메시지를_만들_수_있다() {
+        void HttpResponse_메시지를_만들_수_있다() throws UnsupportedEncodingException {
             // given
             Headers headers = new Headers();
             headers.add(HeaderType.CONTENT_TYPE, "text/html");
             HttpResponse httpResponse = new HttpResponse(new StatusLine("HTTP/1.1", StatusCodeType.OK), headers,
-                    new MessageBody("messageBody"));
+                    new ResponseMessageBody("messageBody".getBytes("UTF-8")));
 
             // when
-            String responseMessage = httpResponse.createResponseMessage();
+            String responseMessage = new String(httpResponse.getResponseBytes());
 
             // then
             assertThat(responseMessage).isEqualTo("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\nmessageBody");
