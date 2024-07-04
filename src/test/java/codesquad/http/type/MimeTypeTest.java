104d14a8f308cf533493ee3770bee7898a29f42a
HiiWee <wpdlzhf159@gmail.com>
2024-07-04 11:05:59 +0900

    refactor: 알 수 없는 mime타입의 경우 application/octet-stream을 반환한다
    - application/octet-stream은 알 수 없는 파일의 경우 content-type으로 지정하도록 되어 있음

--- src/test/java/codesquad/http/type/MimeTypeTest.java
+++ src/test/java/codesquad/http/type/MimeTypeTest.java
@@ -20,10 +20,8 @@ class MimeTypeTest {
     }
 
     @Test
-    void 일치하는_확장자의_mimeType이_없으면_예외가_발생한다() {
+    void 알_수_없는_mimeType이라면_octet_stream이_반환된다() {
         // expect
-        assertThatThrownBy(() -> MimeType.findMimeValue("none_extension"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("none_extension에 해당하는 MIME 타입을 찾을 수 없습니다.");
+        assertThat(MimeType.findMimeValue("undefined-type")).isEqualTo("application/octet-stream");
     }
 }
