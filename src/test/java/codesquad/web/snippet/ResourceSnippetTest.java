76b349b23cd98682ddeed11798d835eede7038a4
HiiWee <wpdlzhf159@gmail.com>
2024-07-16 17:23:33 +0900

    test: 외부 템플릿 파일을 불러올 수 없을때에 대한 테스트 추가
    - 정상적으로 예외를 반환하는지에 대한 테스트가 필요함

--- src/test/java/codesquad/web/snippet/ResourceSnippetTest.java
+++ src/test/java/codesquad/web/snippet/ResourceSnippetTest.java
@@ -1,15 +1,16 @@
 package codesquad.web.snippet;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.io.IOException;
+import codesquad.was.exception.InternalServerException;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class ResourceSnippetTest {
 
     @Test
-    void 외부_템플릿_파일에_스니펫에_문자열_속성을_매핑_할_수_있다() throws IOException {
+    void 외부_템플릿_파일에_스니펫에_문자열_속성을_매핑_할_수_있다() {
         // when
         ResourceSnippet resourceSnippet = new ResourceSnippet(
                 "/test-template.txt",
@@ -18,5 +19,13 @@ class ResourceSnippetTest {
 
         // then
         assertThat(resourceSnippet.getCompleteSnippet()).isEqualTo("Hello World!");
+    }
+
+    @Test
+    void 외부_템플릿_파일이_존재하지_않는다면_예외가_발생한다() {
+        // expect
+        assertThatThrownBy(() -> new ResourceSnippet("/invalid.txt", List.of(new Snippet("Hello"))))
+                .isInstanceOf(InternalServerException.class)
+                .hasMessage("서버에서 요청을 처리할 수 없습니다.");
     }
 }
